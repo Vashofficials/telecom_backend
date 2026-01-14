c7928d9beadccb0bdabef4fee1d1e36b6eb0d62f
Vashofficials <vashtechnical@gmail.com>
2026-01-14 12:11:55 +0530

    sender mail resolved

--- src/utils/sendMail.ts
+++ src/utils/sendMail.ts
@@ -9,12 +9,14 @@ if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false, // Use STARTTLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  logger: true, // Enable logging
+  debug: true,  // Include SMTP traffic in logs
 });
 
 const sendTestEmail = async (
